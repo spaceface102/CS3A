d2a65373530dbbb5820c80dccaa54b5745c438ab
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-07-20 00:00:33 -0700

    Final commit
    Done with the lab. Made a test harness. All output
    went into output.txt other than the std::cout
    specific output

--- LABS/lab16/main.cpp
+++ LABS/lab16/main.cpp
@@ -1,13 +1,94 @@
 #include "matrix.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+
+/****************************************************************
+ * AUTHOR           : Osbaldo Gonzalez Jr.
+ * LAB 16           : Template Matrix
+ * CLASS            : CS 3A
+ * SECTION          : 71206
+ * DUE DATE         : 7/19/2021
+****************************************************************/
+
+/*****************************************************************************
+ * 
+ *   Matrix Template
+ *______________________________________________________________________________
+ *   Test my own template class making a matrix.
+ *______________________________________________________________________________
+ *  INPUT:
+ *      none
+ *  OUTPUT:
+ *      outputFile  : where most of the output is going
+*****************************************************************************/
 
 int main(void)
 {
-    const int size = 40;
-    Matrix<int, 4, 10> m;
-    int y[4][10];
-    int x[size];
+    std::ofstream outputFile;    //OUTPUT - output.txt, store output
 
-    m.setMatrix(y);
-    m.setMatrix(x);
+    outputFile.open("output.txt");
+
+    outputFile << "Demonstrating with string matrix:\n\n";
+    Matrix<std::string, 3, 3> strMtrx;
+    std::string str2Darray[3][3] = {
+        "Matrix set", "to", "first array",
+        "Congra ", "y", " ar",
+        "alm ", "don", " La"
+    };
+    strMtrx.setMatrix(str2Darray);
+    outputFile << strMtrx << "\n\n";
+
+    outputFile
+    << "Matrix incremented by second array\n"
+    << "You're almost done\n"
+    << "with the Lab!\n\n";
+
+    outputFile << "Demonstrating with int matrix:\n";
+    Matrix<int, 2, 3> firstMatrix;
+    int array1D_0[6] = {1, 2, 3, 4, 5, 6};
+    firstMatrix.setMatrix(array1D_0);
+    outputFile << firstMatrix << "\n\n";
+
+    outputFile << "Matrix incremented by second array\n";
+    Matrix<int, 2, 3> secondMatrix;
+    int array2D_0[2][3] = {7, 7, 7, 7, 7, 7};
+    secondMatrix.setMatrix(array2D_0);
+    outputFile << secondMatrix << "\n\n";
+
+    outputFile << "Demonstrating with float matrix:\n\n";
+    outputFile << "Matrix set to first array\n"
+    << "Made two, 2 by 3 matrices and added them together.\n"
+    << "The first one is 1 through 6 and second is 0.6 to 0.1\n"
+    << "Test operator+ and << and setMatrix with a 1D array\n";;
+    double floatarray_0[6] = {1, 2, 3, 4, 5, 6};
+    double floatarray_1[6] = {0.6, 0.5, 0.4, 0.3, 0.2, 0.1};
+    Matrix<double, 2, 3> floatmatrix0;
+    Matrix<double, 2, 3> floatmatrix1;
+    floatmatrix0.setMatrix(floatarray_0);
+    floatmatrix1.setMatrix(floatarray_1);
+    outputFile << floatmatrix0 + floatmatrix1 << "\n\n";
+
+    outputFile 
+    << "Matrix incremented by second array\n"
+    << "Made two, 2 by 3 matrices and added them together.\n"
+    << "The first one is all sevens and the second one is all 0.7s\n"
+    << "Test operator+ and << and setMatrix with a 1D array and 2D array\n";
+    double floatarray_2[2][3] = {7, 7, 7, 7, 7, 7};
+    double floatarray_3[6]    = {0.7, 0.7, 0.7, 0.7, 0.7, 0.7};
+    Matrix<double, 2, 3> floatmatrix2;
+    Matrix<double, 2, 3> floatmatrix3;
+    floatmatrix2.setMatrix(floatarray_2);
+    floatmatrix3.setMatrix(floatarray_3);
+    outputFile << floatmatrix2 + floatmatrix3 << "\n\n";
+
+    outputFile << "Final test, just .setElements and .printMatrix:\n";
+    Matrix<int, 10, 10> upto100;
+    for (int i = 0; i < 10; i++)
+        for (int k = 0; k < 10; k++)
+            upto100.setElement(i, k, (i*10) + 1 + k);
+    outputFile << upto100 << "\n";
+    outputFile << "Same output on console with the printMatrix method!\n";
+    upto100.printMatrix();
+    outputFile.close();
 }
--- LABS/lab16/matrix.h
+++ LABS/lab16/matrix.h
@@ -1,3 +1,11 @@
+/****************************************************************
+ * AUTHOR           : Osbaldo Gonzalez Jr.
+ * LAB 16           : Template Matrix
+ * CLASS            : CS 3A
+ * SECTION          : 71206
+ * DUE DATE         : 7/19/2021
+****************************************************************/
+
 #ifndef MATRIX_CLASS_TEMPLATE_H
 #define MATRIX_CLASS_TEMPLATE_H
 
@@ -37,12 +45,10 @@ public:
  *  Method Constructor: Class Matrix
  *  //DEFAULT CONSTRUCTOR
  * --------------------------------------------------------------
- *  Set all the indicies of the 2D array representing a Matrix
- *  to 0.
+ *  Create a static array for storing values in a Matrix fashion.
  * --------------------------------------------------------------
  *  PRE-CONDITIONS
- *      The templated type T must be able to overload operator=
- *      and accept an integer as argument.
+ *      Must create an object.
  * 
  *  POST-CONDITIONS
  *      The array is ready for the use of any other method
@@ -50,11 +56,7 @@ public:
 ****************************************************************/
 template<typename T, uint64_t Trows, uint64_t Tcols>
 Matrix<T, Trows, Tcols>::Matrix(void)
-{
-    for (uint64_t i = 0; i < Trows; i++)
-        for (uint64_t k = 0; k < Tcols; k++)
-            array[i][k] = 0;
-}
+{}
 //EOF
 
 /****************************************************************
@@ -116,6 +118,25 @@ std::ostream& operator<<(std::ostream& out, const Matrix<T, Trows, Tcols>& m)
 }
 //EOF
 
+/****************************************************************
+ *  //TEMPLATE CLASS
+ *  Method setElement: Class Matrix
+ *  //PUBLIC
+ * --------------------------------------------------------------
+ *  Changes the value of a matrix in one row column pair index.
+ * --------------------------------------------------------------
+ *  PRE-CONDITIONS
+ *      The templated type T must be able to overload 
+ *      the operator=, and should probably do a deep copy if
+ *      need be. Else, unexpected behavior might occur.
+ *
+ *      No range checking is done for the passed in row and col.
+ *      Be careful! 
+ *  POST-CONDITIONS
+ *      The calling object will only be modified at 
+ *      row and column pair defined by the passed in parameters.
+ *      All the other indicies are left untouched.
+****************************************************************/
 template<typename T, uint64_t Trows, uint64_t Tcols>
 void Matrix<T, Trows, Tcols>::setElement(uint64_t row, uint64_t col, T value)
 {
@@ -126,6 +147,7 @@ void Matrix<T, Trows, Tcols>::setElement(uint64_t row, uint64_t col, T value)
 /****************************************************************
  *  //TEMPLATE CLASS
  *  Method setMatrix: Class Matrix
+ *  //OVERLOAD 1
  *  //PUBLIC
  * --------------------------------------------------------------
  *  Fills in the calling matrix object with the values at each
@@ -144,13 +166,17 @@ void Matrix<T, Trows, Tcols>::setElement(uint64_t row, uint64_t col, T value)
  * 
  *      Can't use dynamically allocated arrays.
  * 
- *      
+ *      The size of the passed in array (the number of indicies)
+ *      must be equal to the rows*columns of the calling matrix
+ *      object.
  * 
  *      The templated type T, must define an operator= overload
- *      that passes in another object of the same type.
+ *      that passes in another object of the same type. Probably
+ *      a good idea to make that a deep copy if need.
  * 
  *  POST-CONDITIONS
- *      The calling object will now be filled with the passed 
+ *      The calling object will now be filled with the passed
+ *      in array values at each index.  
 ****************************************************************/
 template<typename T, uint64_t Trows, uint64_t Tcols>
 void Matrix<T, Trows, Tcols>::setMatrix(const T (&array1D)[Trows*Tcols])
@@ -164,6 +190,39 @@ void Matrix<T, Trows, Tcols>::setMatrix(const T (&array1D)[Trows*Tcols])
 }
 //EOF
 
+/****************************************************************
+ *  //TEMPLATE CLASS
+ *  Method setMatrix: Class Matrix
+ *  //OVERLOAD 2
+ *  //PUBLIC
+ * --------------------------------------------------------------
+ *  Fills in the calling matrix object with the values at each
+ *  index by the passed in array
+ * --------------------------------------------------------------
+ *  PRE-CONDITIONS
+ *      The passed in array must be of the same templated type T,
+ *      as that of the object.
+ * 
+ *      The passed in array must be declared in the calling
+ *      function as a static array; size of the array must be
+ *      known at compile time. (Method is passing in the array
+ *      by reference to allow compiler to check array size, if
+ *      they match with the expected size, compiles fine, else
+ *      compilation error).
+ * 
+ *      Can't use dynamically allocated arrays.
+ * 
+ *      Must be a 2D array with the same amount of rows and
+ *      columns as that of the calling object (array2D[rows][cols])
+ * 
+ *      The templated type T, must define an operator= overload
+ *      that passes in another object of the same type. Probably
+ *      a good idea to make that a deep copy if need.
+ * 
+ *  POST-CONDITIONS
+ *      The calling object will now be filled with the passed
+ *      in 2D array values at each index. 
+****************************************************************/
 template<typename T, uint64_t Trows, uint64_t Tcols>
 void Matrix<T, Trows, Tcols>::setMatrix(const T (&array2D)[Trows][Tcols])
 {
@@ -173,6 +232,26 @@ void Matrix<T, Trows, Tcols>::setMatrix(const T (&array2D)[Trows][Tcols])
 }
 //EOF
 
+/****************************************************************
+ *  //TEMPLATE CLASS
+ *  Method operator+: Class Matrix
+ *  //PUBLIC
+ * --------------------------------------------------------------
+ *  Creates a new Matrix object and returns it filled with the
+ *  the result of the calling object plus the the implicitly
+ *  passed in object.
+ * --------------------------------------------------------------
+ *  PRE-CONDITIONS
+ *      The passed in matrix must be of the same type, row, and
+ *      col as the calling object. Else, compiler error.
+ * 
+ *      If not a premtime type, must ensure that type T object
+ *      can overload the operator+.
+ *  POST-CONDITIONS
+ *      Nor the calling or the passed in matrix object are
+ *      modified. New matrix object is returned, that doesn't
+ *      share memory.
+****************************************************************/
 template<typename T, uint64_t Trows, uint64_t Tcols>
 const Matrix<T, Trows, Tcols> Matrix<T, Trows, Tcols>::
 operator+(const Matrix<T, Trows, Tcols>& m) const
